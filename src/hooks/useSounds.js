cf9df145264fe5a90773e191659965413f4e0c92
Moti Elmakies <motiphone2003@gmail.com>
2020-09-27 14:45:26 +0300

    improved audio hook with faster reaction

--- src/hooks/useSounds.js
+++ src/hooks/useSounds.js
@@ -2,29 +2,36 @@ import { isValidElement, useEffect, useState } from 'react';
 
 export const useSounds = () => {
   const [loop, setLoop] = useState(false);
-  const [gameAudio] = useState({
-    title: new Audio('/sounds/title.mp3'),
-    drop: new Audio('/sounds/drop.mp3'),
-    clearRow: new Audio('/sounds/rows_cleared.mp3'),
-    gameOver: new Audio('/sounds/game-over.mp3'),
-    nextLevel: new Audio('/sounds/game-over.mp3'),
-  });
+  const [gameAudio, setGameAudio] = useState({});
   const [currentAudio, setCurrentAudio] = useState(null);
 
   useEffect(() => {
+    setGameAudio({
+      title: new Audio('/sounds/title.mp3'),
+      drop: new Audio('/sounds/drop.mp3'),
+      clearRow: new Audio('/sounds/rows_cleared.mp3'),
+      gameOver: new Audio('/sounds/game-over.mp3'),
+      nextLevel: new Audio('/sounds/game-over.mp3'),
+    });
+  }, []);
+  useEffect(() => {
     console.log(' use effect teteris');
+
     if (currentAudio != null) {
       gameAudio[currentAudio].loop = loop;
     }
   }, [loop]);
 
   const playSound = (audio, isLoop, volume = 1) => {
+    setVolume(volume);
     setCurrentAudio(audio);
     setLoop(isLoop);
     if (!gameAudio[audio]) return;
     gameAudio[audio].volume = volume;
-    const test = gameAudio[audio].play();
-    test.then(() => console.log('promise'));
+    gameAudio[audio].pause();
+    gameAudio[audio].currentTime = 0;
+    gameAudio[audio].play();
+
     console.log('play');
   };
 
@@ -38,7 +45,12 @@ export const useSounds = () => {
     if (currentAudio !== null) {
       let currentValue = gameAudio[currentAudio].volume;
       const reduceValueInterval = setInterval(() => {
-        currentValue -= 0.1;
+        if (currentValue > 0.1) {
+          currentValue -= 0.1;
+        } else {
+          volume = currentValue;
+        }
+
         gameAudio[currentAudio].volume = currentValue;
 
         if (currentValue <= volume) {
